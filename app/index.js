b1d18944637d9f905dbe49f0dd9d378decb4da7c
Anssikka <Anssikka@gmail.com>
2019-09-27 19:52:13 +0300

    navbaria ja tyylittelyä

---
+++ app/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+import './index.css'
+import Popular from './components/Popular'
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Popular />
+      </div>
+    )
+  }
+}
+
+ReactDom.render(<App />, document.getElementById('app'))
+
