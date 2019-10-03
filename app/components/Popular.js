b89380aa0da504d117ca86f2b3fa82b831c60765
Anssikka <Anssikka@gmail.com>
2019-10-03 15:37:06 +0300

    feat: reset battlers and alot of refactoring

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { fetchPopularRepos} from '../utils/api'
 import { FaUser, FaStar, FaCodeBranch, FaExclamationTriangle } from 'react-icons/fa'
+import Card from './Card'
 
 function LanguagesNav ({selected, onUpdateLanguage}) {
   const languages = ['All', 'Javascript', 'Ruby', 'Java', 'CSS', 'Python']
@@ -26,24 +27,27 @@ LanguagesNav.propTypes = {
   onUpdateLanguage: PropTypes.func.isRequired,
 }
 
-  function ReposGrid ({ repos }) {
-    return (
-      <ul className='grid space-around'>
-        {repos.map((repo, index) => {
-          const { name, owner, html_url, stargazers_count, forks, open_issues} = repo
-          const { login, avatar_url } = owner
+function ReposGrid({ repos }) {
+  return (
+    <ul className='grid space-around'>
+      {repos.map((repo, index) => {
+        const { name, owner, html_url, stargazers_count, forks, open_issues } = repo
+        const { login, avatar_url } = owner
 
-          return (
-            <li key = {html_url} className = 'repo bg-light'>
-              <h4 className='header-lg center-text'> #{index + 1} </h4>
-              <img src={avatar_url} className='avatar' alt={`Avatar for ${login}`}/>
-              <h2 className='center-text'>
-                <a className='link' href={html_url}>{login}</a>
-              </h2>
+        return (
+          <li key={html_url}>
+            <Card
+              header={`#${index + 1}`}
+              avatar={avatar_url}
+              href={html_url}
+              name={login}
+            >
               <ul className='card-list'>
                 <li>
                   <FaUser color='rgb(255, 191, 116)' size={22} />
-                  <a href={`https://github.com/${login}`}>{login}</a>
+                  <a href={`https://github.com/${login}`}>
+                    {login}
+                  </a>
                 </li>
                 <li>
                   <FaStar color='rgb(255, 215, 0)' size={22} />
@@ -55,19 +59,19 @@ LanguagesNav.propTypes = {
                 </li>
                 <li>
                   <FaExclamationTriangle color='rgb(241, 138, 147)' size={22} />
-                  {open_issues.toLocaleString()} open issues
+                  {open_issues.toLocaleString()} open
                 </li>
               </ul>
-            </li>
-          )
-        })}
-        
-      </ul>
-    )
-  }
+            </Card>
+          </li>
+        )
+      })}
+    </ul>
+  )
+}
 
 ReposGrid.propTypes = {
-  repos: PropTypes.array.isRequired
+  repos: PropTypes.array.isRequired,
 }
 
 export default class Popular extends React.Component {
