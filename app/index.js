6354fc8a57cf5e4e024fdb70fd9ab12c8cfb600e
Anssikka <Anssikka@gmail.com>
2019-10-04 16:44:06 +0300

    feat: added basic react router routing

--- app/index.js
+++ app/index.js
@@ -3,19 +3,40 @@ import ReactDom from 'react-dom'
 import './index.css'
 import Popular from './components/Popular'
 import Battle from './components/Battle'
+import Nav from './components/Nav'
+import { ThemeProvider } from './contexts/theme'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      theme: 'light',
+      toggleTheme: () => {
+        this.setState(({theme}) => ({
+          theme: theme === 'light' ? 'dark' : 'light'
+        }))
+      }
+    }
+  } 
+
   render() {
     return (
-      <div className='container'>
-        <Battle />
-        
-        
-      </div>
+      <Router>
+        <ThemeProvider value={this.state} >
+        <div className={this.state.theme} >
+          <div className='container'>
+            <Nav />
+              <Route exact path='/' component={Popular} />
+              <Route path='/battle' component={Battle} />
+          </div>
+        </div>
+        </ThemeProvider>
+      </Router>
     )
   }
 }
 
 ReactDom.render(<App />, document.getElementById('app'))
-
