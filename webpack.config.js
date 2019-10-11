0358384aaf9250bec1abc0ed1de064a5e9d89280
Anssikka <Anssikka@gmail.com>
2019-10-11 13:21:22 +0300

    config update

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
 
       ]
     },
-    mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+    mode: 'production',
     plugins: [
       new HtmlWebpackPlugin({
         template: 'app/index.html'
